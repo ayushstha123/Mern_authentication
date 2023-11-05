80ef5712ad3bdd32875e2f3847fcb9ec2861086b
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-11-05 18:06:22 +0545

    connected to db
    successfully connected to mongo db bt installing mongoose and dotenv (for storing useful and secret information)

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 const PORT = 3000;
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to MONGODB");
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
