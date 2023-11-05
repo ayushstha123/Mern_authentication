cd73a3667f6d4983d126c5e546f1ac0f8ad2b24d
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-11-05 10:07:55 +0545

    Second commit
    adding routes and creating pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
-  return (
-    <div className=''>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/About" element={<About/>}></Route>
+    <Route path="/Profile" element={<Profile/>}></Route>
+    <Route path="/SignIn" element={<SignIn/>}></Route>
+    <Route path="/SignUp" element={<SignUp/>}></Route>
+
+  </Routes>
+   </BrowserRouter>
 }
 
 export default App
